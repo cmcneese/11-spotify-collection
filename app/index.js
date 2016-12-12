191c668db67974aeaa873ef39f7e2a752e45185e
Chris McNeese <christophermcneese@mac.com>
2016-12-12 00:44:33 -0600

    add model and view

--- app/index.js
+++ app/index.js
@@ -1,2 +1,14 @@
 import 'whatwg-fetch';
-import 'katy';
+import data from './katy.json';
+import Model from './spotify-collection/app/model';
+import View from './spotify-collection/app/view';
+
+data.tracks.items.forEach((item) =>  {
+  const model = new Model(item);
+
+  const view = new View(model);
+
+  const results = document.querySelector('.results');
+  results.appendChild(view.track);
+  view.render();
+});
