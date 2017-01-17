baea3134444cd1ff34129c2d72b5ebbec6b765a1
Chris McNeese <christophermcneese@mac.com>
2017-01-17 17:11:18 -0600

    fix url issue final time

--- app/Spotify-Track/model.js
+++ app/Spotify-Track/model.js
@@ -5,6 +5,6 @@ export default class Model {
     this.artist = track.artists[0].name;
     this.name = track.name;
     this.image = track.album.images[0].url;
-    this.model = track.external_urls.spotify;
+    this.url = track.external_urls.spotify;
   }
 }
